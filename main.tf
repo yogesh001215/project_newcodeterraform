b132a1189a530f3ebbd13f38c491c8bc52b44ce7
Yogesh sharma <eryogeshrf@gmail.com>
2024-11-17 18:17:21 +0530

    2nd new update

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg1_remote" {
+  name     = "rg_code_v1"
+  location = "West Europe"
+}
